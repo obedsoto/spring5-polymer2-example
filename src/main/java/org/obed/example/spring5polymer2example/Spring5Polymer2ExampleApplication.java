b3d780faedde1bdf1e7ae8f0397383ca94d367dc
Obed Soto <obedsoto@ge.com>
2018-03-27 21:35:14 -0600

    First commit, pom.xml and bower.json files

---
+++ src/main/java/org/obed/example/spring5polymer2example/Spring5Polymer2ExampleApplication.java
@@ -0,0 +1,12 @@
+package org.obed.example.spring5polymer2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5Polymer2ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5Polymer2ExampleApplication.class, args);
+	}
+}
